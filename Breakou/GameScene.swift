0573030ba063223634e2bfd3ce2c5cdc1dfd4555
Kevin Rosas <rosaskevin044@gmail.com>
2021-03-15 10:35:52 -0500

    Initial Commit

---
+++ Breakou/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  Breakou
+//
+//  Created by kevin rosas on 3/15/21.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+        
+    override func didMove(to view: SKView) {
+        //This stuff happpens once {when the app opens}
+        }
+    
+}
